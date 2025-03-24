fe046f20cb1a5e5985f3c2a446134c4311744dc1
Felix Soubelet <felix.soubelet@protonmail.com>
2025-03-24 10:05:18 +0100

    transfer elegant test here

--- tests/test_ibs_equilibrium.py
+++ tests/test_ibs_equilibrium.py
@@ -3,7 +3,6 @@ import xobjects as xo
 import xtrack as xt
 from ibs_conftest import XTRACK_TEST_DATA
 
-
 BUNCH_INTENSITY: float = 6.2e9  # 1C bunch intensity
 
 
@@ -44,7 +43,6 @@ def test_equilibrium_vs_analytical_constraint_coupling(
     an equilibrium with SR and IBS, in the case where we enforce
     a betatron coupling constraint on the transverse planes. The
     resulting values are tested against an analytical estimate.
-    TODO: ref for the analytical formula?
     """
     # -------------------------------------------
     # Get the twiss with SR effects from the configured line
@@ -85,7 +83,6 @@ def test_equilibrium_vs_analytical_constraint_excitation(
     an equilibrium with SR and IBS, in the case where we enforce
     an excitation type constraint on the transverse planes. The
     resulting values are tested against an analytical estimate.
-    TODO: ref for the analytical formula?
     """
     # -------------------------------------------
     # Get the twiss with SR effects from the configured line
@@ -129,7 +126,6 @@ def test_equilibrium_vs_analytical_no_constraint(
     becomes almost identical to the solution of the differential
     equation describing the emittance evolution in presence of IBS
     and SR.
-    TODO: ref for the analytical formula?
     """
     # -------------------------------------------
     # Get the twiss with SR effects from the configured line
@@ -170,6 +166,100 @@ def test_equilibrium_vs_analytical_no_constraint(
     # Check against ELEGANT results for this scenario
     # These are hardcoded (from ELEGANT version with
     # corrected partition numbers use)
+
+
+# ----- Test Functions vs ELEGANT Results ----- #
+
+# Ugly hardcoded results from ELEGANT. The keys here
+# are emittance coupling factor and associated are the
+# corresponding final values we want to compare to.
+ELEGANT_RESULTS: dict[float, dict[str, float]] = {
+    0.02: {
+        "eps_x": 1.88319e-10,
+        "sigma_z": 0.00491982,
+        "sigma_delta": 0.00140017,
+        "T_x": 106.648,
+        "T_z": 32.8772,
+    },
+    0.1: {
+        "eps_x": 1.46119e-10,
+        "sigma_z": 0.00441742,
+        "sigma_delta": 0.00125719,
+        "T_x": 81.5391,
+        "T_z": 25.2408,
+    },
+    0.5: {
+        "eps_x": 1.05835e-10,
+        "sigma_z": 0.00404997,
+        "sigma_delta": 0.00115261,
+        "T_x": 63.7394,
+        "T_z": 17.7664,
+    },
+    1: {
+        "eps_x": 8.5549e-11,
+        "sigma_z": 0.00395442,
+        "sigma_delta": 0.00112542,
+        "T_x": 63.1134,
+        "T_z": 15.4736,
+    },
+}
+
+
+@pytest.mark.parametrize("emittance_coupling_factor", [0.02, 0.1, 0.5, 1])
+def test_equilibrium_vs_elegant(emittance_coupling_factor, bessy3_line_with_radiation: xt.Line):
+    """
+    Load the BESSY III line and compute ierations until we reach
+    an equilibrium with SR and IBS, in the case where we enforce
+    a betatron coupling constraint on the transverse planes. The
+    resulting values are tested against elegant results under the
+    same conditions.
+
+    Note
+    ----
+        The elegant results are expected (and accepted) to be
+        slightly different due to small differences in the TWISS
+        functions, IBS growth rates etc themselves.
+    """
+    # -------------------------------------------
+    # Get the twiss with SR effects from the configured line
+    tw = bessy3_line_with_radiation.twiss(eneloss_and_damping=True)
+    # -------------------------------------------
+    # Compute the equilibrium emittances - coupling constraint
+    result = tw.compute_equilibrium_emittances_from_sr_and_ibs(
+        formalism="Nagaitsev",  # No Dy in the line, faster
+        total_beam_intensity=BUNCH_INTENSITY,
+        emittance_coupling_factor=emittance_coupling_factor,
+        emittance_constraint="coupling",
+    )
+    # -------------------------------------------
+    # Check Xfields results vs ELEGANT results
+    # Check horizontal equilibrium emittance and IBS growth rate
+    xo.assert_allclose(
+        result.eq_sr_ibs_gemitt_x,
+        ELEGANT_RESULTS[emittance_coupling_factor]["eps_x"],
+        atol=1e-12,
+        rtol=5e-2,
+    )
+    # Factor of 2 because different conventions between Xsuite and elegant!
+    xo.assert_allclose(
+        result.Kx[-1],
+        ELEGANT_RESULTS[emittance_coupling_factor]["T_x"] / 2,
+        rtol=6e-2,
+    )
+    # Check longitudinal equilibrium emittance and IBS growth rate
+    # Different eps_zeta convention between both codes
+    xo.assert_allclose(
+        result.eq_sr_ibs_gemitt_zeta,
+        ELEGANT_RESULTS[emittance_coupling_factor]["sigma_delta"]
+        * ELEGANT_RESULTS[emittance_coupling_factor]["sigma_z"],
+        rtol=5e-2,
+    )
+    # Factor of 2 because different conventions between Xsuite and elegant!
+    xo.assert_allclose(
+        result.Kz[-1],
+        ELEGANT_RESULTS[emittance_coupling_factor]["T_z"] / 2,
+        rtol=6e-2,
+    )
 
 
 # ----- Some Test Functions for Behaviour ----- #
