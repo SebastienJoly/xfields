29711b34e2d32d0cf93b6f2c6cc47b19fd8c921f
Felix Soubelet <felix.soubelet@protonmail.com>
2025-04-02 17:02:27 +0200

    remove analytical here

--- examples/005_ibs/006_steady_state_emittances_excitation.py
+++ examples/005_ibs/006_steady_state_emittances_excitation.py
@@ -60,31 +60,6 @@ result = tw.compute_equilibrium_emittances_from_sr_and_ibs(
 print(result)
 # TODO: output of the print
 
-######################################
-# Comparison with analytical results #
-######################################
-
-# These are analytical estimate (from the last step's IBS growth rates)
-analytical_x = result.gemitt_x[0] / (1 - result.Kx[-1] / (tw.damping_constants_s[0]))
-analytical_y = result.gemitt_y[0] / (1 - result.Kx[-1] / (tw.damping_constants_s[0]))
-analytical_z = result.gemitt_zeta[0] / (1 - result.Kz[-1] / (tw.damping_constants_s[2]))
-
-print()
-print("Emittance Constraint: Excitation")
-print("Horizontal steady-state emittance:")
-print("---------------------------------")
-print(f"Analytical: {analytical_x}")
-print(f"ODE:        {result.eq_sr_ibs_gemitt_x}")
-print("Vertical steady-state emittance:")
-print("-------------------------------")
-print(f"Analytical: {analytical_y}")
-print(f"ODE:        {result.eq_sr_ibs_gemitt_y}")
-print("Longitudinal steady-state emittance:")
-print("-----------------------------------")
-print(f"Analytical: {analytical_z}")
-print(f"ODE:        {result.eq_sr_ibs_gemitt_zeta}")
-
-# TODO: add print here with the outputs
 
 # The results from the table can easily be plotted to view
 # at the evolution of various parameters across time steps
@@ -96,12 +71,9 @@ fig, (ax0, ax1) = plt.subplots(2, 1, sharex=True, layout="constrained")
 
 (l1,) = ax0.plot(result.time * 1e3, result.gemitt_x * 1e12, ls="-", label=r"$\tilde{\varepsilon}_x$")
 (l2,) = ax0.plot(result.time * 1e3, result.gemitt_y * 1e12, ls="--", label=r"$\tilde{\varepsilon}_y$")
-l4 = ax0.axhline(analytical_x * 1e12, color="C0", ls="-.", label=r"Analytical $\varepsilon_{x}^{eq}$")
-l5 = ax0.axhline(analytical_y * 1e12, color="C1", ls="-.", label=r"Analytical $\varepsilon_{y}^{eq}$")
 ax0b = ax0.twinx()
 (l3,) = ax0b.plot(result.time * 1e3, result.gemitt_zeta * 1e6, color="C2", label=r"$\varepsilon_z$")
-l6 = ax0b.axhline(analytical_z * 1e6, color="C2", ls="-.", label=r"Analytical $\varepsilon_{\zeta}^{eq}$")
-ax0.legend(handles=[l1, l2, l3, l4, l5], ncols=2)
+ax0.legend(handles=[l1, l2, l3], ncols=2)
 
 ax1.plot(result.time * 1e3, result.Kx, label=r"$\alpha_{x}^{IBS}$")
 ax1.plot(result.time * 1e3, result.Ky, label=r"$\alpha_{y}^{IBS}$")
@@ -109,7 +81,7 @@ ax1.plot(result.time * 1e3, result.Kz, label=r"$\alpha_{z}^{IBS}$")
 ax1.legend()
 
 ax1.set_xlabel("Time [ms]")
-ax0.set_ylabel(r"$\tilde{\varepsilon_{x,y}}$ [pm.rad]")
+ax0.set_ylabel(r"$\tilde{\varepsilon}_{x,y}$ [pm.rad]")
 ax0b.set_ylabel(r"$\varepsilon_{\zeta}$ [m]")
 ax1.set_ylabel(r"$\alpha^{IBS}$ [$s^{-1}$]")
 fig.align_ylabels((ax0, ax1))
